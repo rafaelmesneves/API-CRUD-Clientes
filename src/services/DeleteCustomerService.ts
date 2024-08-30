5b130238bfe42f8bbbe08db26f4a85208e926336
Rafael Mesquita <118580433+rafaelmesneves@users.noreply.github.com>
2024-08-30 12:46:05 -0300

    Back end

---
+++ src/services/DeleteCustomerService.ts
@@ -0,0 +1,34 @@
+import prismaClient from "../prisma"
+
+interface DeleteCustomerProps{
+    id: string
+}
+
+class DeleteCustomerService{
+    async execute({id}: DeleteCustomerProps){
+
+        if(!id){
+            throw new Error("Solicitacao invalida")
+        }
+
+        const findCustomer = await prismaClient.customer.findFirst({
+            where:{
+                id: id
+            }
+        })
+
+        if(!findCustomer){
+            throw new Error("Cliente nao existe!")
+        }
+
+        await prismaClient.customer.delete({
+            where:{
+                id: findCustomer.id
+            }
+        })
+
+        return {message: "Deletado com sucesso!"}
+    }
+}
+
+export {DeleteCustomerService}
